2026135b6636238f2e7d96b1680c4dc4186914cf
avivga <avivga@gmail.com>
2019-03-12 13:35:02 +0200

    add convert action

--- face_swap.py
+++ face_swap.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import random
+import imageio
 
 import numpy as np
 
@@ -70,6 +72,38 @@ def train(args):
 	face_converter.save(model_dir)
 
 
+def convert(args):
+	assets = AssetManager(args.base_dir)
+	model_dir = assets.get_model_dir(args.model_name)
+	preprocessed_dir = assets.get_preprocess_dir(args.data_name)
+	prediction_dir = assets.create_prediction_dir(args.model_name)
+
+	face_converter = FaceConverter.load(model_dir)
+
+	imgs = dict()
+	for identity_file_name in os.listdir(preprocessed_dir):
+		identity_id = os.path.splitext(identity_file_name)[0]
+		identity_imgs = np.load(os.path.join(preprocessed_dir, identity_file_name))['imgs']
+		imgs[identity_id] = identity_imgs.astype(np.float64) / 255
+
+	identity_ids = list(imgs.keys())
+	for i in range(args.num_of_samples):
+		source_identity_id = random.choice(identity_ids)
+		idx = np.random.randint(0, imgs[source_identity_id].shape[0], size=1)
+		source_identity_img = imgs[source_identity_id][idx]
+
+		target_identity_id = random.choice(identity_ids)
+		idx = np.random.randint(0, imgs[target_identity_id].shape[0], size=1)
+		target_identity_img = imgs[target_identity_id][idx]
+
+		converted_img = face_converter.converter.predict([
+			source_identity_img, target_identity_img
+		])
+
+		converted_merged_img = np.concatenate((source_identity_img[0], target_identity_img[0], converted_img[0]), axis=1)
+		imageio.imwrite(os.path.join(prediction_dir, '%d.png' % i), converted_merged_img)
+
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-bd', '--base-dir', type=str, required=True)
@@ -98,11 +132,11 @@ def main():
 	train_parser.add_argument('-g', '--gpus', type=int, default=1)
 	train_parser.set_defaults(func=train)
 
-	# convert_parser = action_parsers.add_parser('convert')
-	# convert_parser.add_argument('-dn', '--data-name', type=str, required=True)
-	# convert_parser.add_argument('-mn', '--model-name', type=str, required=True)
-	# convert_parser.add_argument('-ns', '--num-of-samples', type=int, required=True)
-	# convert_parser.set_defaults(func=convert)
+	convert_parser = action_parsers.add_parser('convert')
+	convert_parser.add_argument('-dn', '--data-name', type=str, required=True)
+	convert_parser.add_argument('-mn', '--model-name', type=str, required=True)
+	convert_parser.add_argument('-ns', '--num-of-samples', type=int, required=True)
+	convert_parser.set_defaults(func=convert)
 
 	args = parser.parse_args()
 	args.func(args)
