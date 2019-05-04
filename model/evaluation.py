90273335384e8bfea6042e494e120ed75f30e91c
avivga <avivga@gmail.com>
2019-05-04 08:31:01 +0300

    plot original images during evaluation

--- model/evaluation.py
+++ model/evaluation.py
@@ -33,20 +33,21 @@ class EvaluationCallback(TensorBoard):
 		reference_identity_img_ids = np.where(self.__identities == reference_identity)[0]
 		img_ids = np.random.choice(reference_identity_img_ids, size=self.__n_poses, replace=False)
 
+		imgs = self.__imgs[img_ids]
 		pose_codes = self.__pose_embedding.predict(img_ids)
 		identity_codes = self.__identity_embedding.predict(identities)
 		identity_adain_params = self.__identity_modulation.predict(identity_codes)
 
-		rows = []
+		output = [np.concatenate(list(imgs), axis=1)]
 		for i in range(self.__n_identities):
-			row = []
-			for j in range(self.__n_poses):
-				img = self.model.predict([pose_codes[[j]], identity_adain_params[[i]]])[0]
-				row.append(img)
+			identity_imgs = [
+				self.model.predict([pose_codes[[j]], identity_adain_params[[i]]])[0]
+				for j in range(self.__n_poses)
+			]
 
-			rows.append(np.concatenate(row, axis=1))
+			output.append(np.concatenate(identity_imgs, axis=1))
 
-		merged_img = np.concatenate(rows, axis=0)
+		merged_img = np.concatenate(output, axis=0)
 
 		summary = tf.Summary(value=[tf.Summary.Value(tag='sample', image=self.make_image(merged_img))])
 		self.writer.add_summary(summary, global_step=epoch)
