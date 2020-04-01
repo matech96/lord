e980dc36baa49fc8975f8f70e6508969df60a233
Ákos Gángoly <gangoly96@gmail.com>
2020-04-01 22:43:29 +0200

    bugfix

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ default_config = dict(
 	content_decay=0.001,
 
 	n_adain_layers=4,
-	adain_enabled=True,
+	# adain_enabled=True,
 	adain_dim=256,
-	adain_normalize=True,
+	# adain_normalize=True,
 
 	perceptual_loss=dict(
 		layers=[2, 5, 8, 13, 18],
